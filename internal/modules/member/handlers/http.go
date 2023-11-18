0efbea1d0c0e9d6f028149aabae31e1648ec31c3
Kang <63038044+pstpmn@users.noreply.github.com>
2023-11-18 21:58:20 +0700

    prepare hanlder

--- internal/modules/member/handlers/http.go
+++ internal/modules/member/handlers/http.go
@@ -1,1 +1,27 @@
-package handlers
+package memberHandler
+
+import (
+	"super-number-simple-microservice/internal/modules/member"
+	"super-number-simple-microservice/pkg"
+
+	echo "github.com/labstack/echo/v4"
+)
+
+type (
+	IHttpHandler interface {
+		Registration(c echo.Context) error
+	}
+	httpHandler struct {
+		Response   pkg.IResponse
+		MemUseCase member.IUseCase
+	}
+)
+
+// Registration implements IHttpHandler.
+func (*httpHandler) Registration(c echo.Context) error {
+	panic("unimplemented")
+}
+
+func NewHttpHandler(memUseCase member.IUseCase, response pkg.IResponse) IHttpHandler {
+	return &httpHandler{}
+}
